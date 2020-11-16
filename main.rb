54921c71fb8b3c515907285c5e3f145644b24fa4
Brennen Karp <brennenk45@gmail.com>
2020-11-16 09:31:07 -0800

    Only commit

--- main.rb
+++ main.rb
@@ -5,9 +5,9 @@ require_relative 'rental'
 
 items = [:apple, :water, :protein_bar]
 
-bike = Bike.new(1, :pink, 99.99, items)
+bike = Bike.new(1, :pink, 99.99)
 
-rental = Rental.new(bike)
+rental = Rental.new(bike, items)
 
 puts "Total Price: #{rental.price}, Total Weight: #{rental.weight}"
 
--- rental.rb
+++ rental.rb
@@ -1,17 +1,19 @@
 class Rental
 
-  attr_reader :bike
-
-  def initialize(bike)
+  require_relative 'luggage'
+  attr_reader :bike, :luggage
+  
+  def initialize(bike, extra_items)
     @bike = bike
+    @luggage = Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, extra_items, bike)
   end
 
   def price
-    self.bike.price + self.bike.luggage.items.count * 10
+    self.bike.price + (self.bike.weight * 2) + (self.luggage.weight * 2)
   end
 
   def weight
-    self.bike.weight + self.bike.luggage.items.count
+    self.bike.weight + self.luggage.weight
   end
 
 end
